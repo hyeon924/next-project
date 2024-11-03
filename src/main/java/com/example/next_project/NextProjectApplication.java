0717fcddd355df4e8ad1cc9f202e59b6a133a3d6
hyeon924 <hyeonn4713@gmail.com>
2024-11-03 20:19:51 +0900

    first commit

---
+++ src/main/java/com/example/next_project/NextProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.next_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/next_project/NextProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.next_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
